8c5418be0aeb91b71f591d596f9cb2fe475c2a5c
trishalisawant <trishalisawant12@gmail.com>
2026-04-20 08:37:38 +0530

    Create 2078-Two Furthest Houses With Different Colors.cpp

---
+++ 2078-Two Furthest Houses With Different Colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int maxdist = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+                if(colors[i]!=colors[j])
+                {
+                    maxdist = max(maxdist,(j-i));
+                    break;
+                }
+            }
+        }
+        return maxdist;
+    }
+};
