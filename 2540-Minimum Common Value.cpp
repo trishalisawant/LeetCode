fd9793117c9674c1fabedb02098897476a016d27
trishalisawant <trishalisawant12@gmail.com>
2026-05-19 10:18:10 +0530

    Create 2540-Minimum Common Value.cpp

---
+++ 2540-Minimum Common Value.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+       set<int>s;
+       for(int i=0;i<nums1.size();i++)
+       {
+         s.insert(nums1[i]);
+       }
+       for(int j=0;j<nums2.size();j++)
+       {
+         if(s.count(nums2[j]))
+         {
+            return nums2[j];
+         }
+       }
+       return -1;
+    }
+};
