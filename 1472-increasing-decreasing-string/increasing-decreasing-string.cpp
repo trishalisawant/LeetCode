bc0f80b6b84ecbdfc40b070358fb6673ac21f5f4
trishalisawant <trishalisawant12@gmail.com>
2026-08-13 18:23:02 +0530

    Time: 7 ms (16.60%) | Memory: 10.2 MB (23.40%) - LeetSync

---
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string sortString(string s) {
+        vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        string result = "";
+
+        while (result.size() < s.size()) {
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] > 0) {
+                    result += char(i + 'a');
+                    freq[i]--;
+                }}
+            for (int i = 25; i >= 0; i--) {
+                if (freq[i] > 0) {
+                    result += char(i + 'a');
+                    freq[i]--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
