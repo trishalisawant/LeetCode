d3fd06cbe22b2789a609d05f0e2eff832546b787
trishalisawant <trishalisawant12@gmail.com>
2026-09-19 09:41:41 +0530

    Time: 3 ms (75.89%) | Memory: 18.8 MB (6.08%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                if (s[left] < s[right]) {
+                    s[right] = s[left];
+                }
+                else {
+                    s[left] = s[right];
+                }
+            }
+
+            left++;
+            right--;
+        }
+
+        return s; 
+    }
+};
